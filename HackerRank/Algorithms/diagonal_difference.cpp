ff44406659dbe18b28f2b1b3192de2d2a36cbe24
firedranzer <rupeshharode@gmail.com>
2017-11-21 20:10:08 +0530

    added program diagonal difference

--- HackerRank/Algorithms/diagonal_difference.cpp
+++ HackerRank/Algorithms/diagonal_difference.cpp
@@ -9,16 +9,19 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    int A[1000];
+    int A[1000][1000];
+    int sumd1 = 0;
+    int sumd2 = 0;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin>>A[i][j];
-        }
+        
         if(i==j){
             sumd1 += A[i][j];
         }
-        if(i+j=n-1){
+        if(i+j==n-1){
             sumd2 += A[i][j]; 
+        }
         }
     }
     int sum = sumd1 - sumd2;
