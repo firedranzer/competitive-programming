e3ea5394b6ef856d894ec99a33c34d4e51e95593
firedranzer <rupeshharode@gmail.com>
2017-11-21 20:08:28 +0530

    working

---
+++ HackerRank/Algorithms/diagonal_difference.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int A[1000];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>A[i][j];
+        }
+        if(i==j){
+            sumd1 += A[i][j];
+        }
+        if(i+j=n-1){
+            sumd2 += A[i][j]; 
+        }
+    }
+    int sum = sumd1 - sumd2;
+    if(sum < 0){
+        sum = sum * -1;
+    }
+    cout<<sum;
+    return 0;
+}
