ae2c18835330a93a11b7b77a3b21ed7e0d99da05
firedranzer <rupeshharode@gmail.com>
2017-11-22 12:05:07 +0530

    modified counting_valleys.cpp

--- HackerRank/Algorithms/counting_valleys.cpp
+++ HackerRank/Algorithms/counting_valleys.cpp
@@ -12,11 +12,20 @@ int main() {
     cin>>n;
     int valley=0;
     int count=0;
+    bool belowSea = false;
     for(int i=0; i<n; i++){
         cin>>ch;
         if(ch=='U')     count++;
         if(ch=='D')     count--;
         if(count==0)    valley++;
+        if(!belowSea && count < 0)
+            {
+                valley++;
+                belowSea = true;
+            }
+            
+            if(count >= 0)
+                belowSea = false;
     }
     cout<<valley;
     return 0;
