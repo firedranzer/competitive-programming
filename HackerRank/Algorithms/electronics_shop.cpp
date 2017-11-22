45596e369ee2d4a1366b6b042e82cfc8e77a8829
firedranzer <rupeshharode@gmail.com>
2017-11-22 13:58:16 +0530

    added program electronics_shop.cpp

---
+++ HackerRank/Algorithms/electronics_shop.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int s, n, m;
+    cin>>s>>n>>m;
+    int key[1000], usb[1000];
+    for(int i=0; i<n; i++){
+        cin>>key[i];
+    }
+    for(int i=0; i<m; i++){
+        cin>>usb[i];
+    }
+     
+    return 0;
+}
