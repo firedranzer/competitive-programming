8e0401509ab3257193e9f2232a18982bbc6e10e1
firedranzer <rupeshharode@gmail.com>
2017-11-23 23:37:05 +0530

    modified program electronics_shop

--- HackerRank/Algorithms/electronics_shop.cpp
+++ HackerRank/Algorithms/electronics_shop.cpp
@@ -8,14 +8,26 @@ using namespace std;
 
 int main() {
     int s, n, m;
-    cin>>s>>n>>m;
     int key[1000], usb[1000];
+    int sum[1000];
+    cin>>s>>n>>m;
     for(int i=0; i<n; i++){
         cin>>key[i];
     }
     for(int i=0; i<m; i++){
         cin>>usb[i];
     }
-     
+    sum[0] = key[0] + usb[0];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            sum[i] = key[i]+usb[j];
+        }
+    }
+    int max=sum[0]; 
+    for(int i=0; i<n*m; i++){
+        if(max<sum[i]&&sum[i]<=s)  max=sum[i];
+    }
+    if(max>s){  cout<<"-1";}
+    else    cout<<max;
     return 0;
 }
