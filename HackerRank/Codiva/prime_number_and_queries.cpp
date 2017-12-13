ae719cf6a62d2a593b2618defa00d678ed5c7e5d
firedranzer <rupeshharode@gmail.com>
2017-12-13 17:27:15 +0530

    modeified program prime_number_and_queries.cpp

--- HackerRank/Codiva/prime_number_and_queries.cpp
+++ HackerRank/Codiva/prime_number_and_queries.cpp
@@ -4,43 +4,54 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+long int array_min[1000005], array_max[1000005];
 
 int main() {
-    int low, high, i, flag, t;
-    int k=0;
-    int a[10000];
+    
+    //Sieve of eratosthenes ---- Simple Sieve
+    bool  prime[1000005]; 
+    memset(prime, true, sizeof(prime));
+ 
+    for (int p=2; p*p<1000005; p++)
+    {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples of p
+            for (int i=p*2; i<1000005; i += p)
+                prime[i] = false;
+        }
+    }
+
+    // End of Sieve
+    // array_min contains smallest prime numbers wrt index.
+    array_min[1]=1;
+    // array_min contains smallest prime numbers wrt index.
+    array_max[1000004]=1000004;
+
+    for(int i=2; i<1000005; i++){
+        if(prime[i]){
+            array_min[i] = i;
+        }
+        else{
+            array_min[i]=array_min[i-1];
+        }
+    }
+
+    for(int i=1000004; i>1; i--){
+        if(prime[i]){
+            array_max[i] = i;
+        }
+        else{
+            array_max[i]=array_max[i+1];
+        }
+    }
+    int t;
     cin>>t;
     while(t--){
-    cin >> low >> high;
-    int min=low;
-    int max=low;
-    while (low <= high)
-    {
-        flag = 0;
-
-        for(i = 2; i <= low/2; ++i)
-        {
-            if(low % i == 0)
-            {
-                flag = 1;
-                break;
-            }
-        }
-
-        if (flag == 0){
-            a[k]=low;
-            if(a[k]<min){   min=a[k];}
-            if(a[k]>max){   max=a[k];}
-            k=k+1;
-        }
-        
-
-        ++low;
+        int x, y;
+        cin>>x>>y;
+        if(array_min[y]<x || array_max[x]>y){   cout<<"0\n";}
+        else{   cout<<array_min[y]-array_max[x]<<"\n";}
     }
-    int diff = max - min;
-        cout<<diff<<endl;
-    }
-
-    return 0;
 }
