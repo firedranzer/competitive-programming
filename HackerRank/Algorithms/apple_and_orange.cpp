04b90691eb39d33dc2b76f5652c674e78ac5df32
firedranzer <rupeshharode@gmail.com>
2017-12-14 12:50:20 +0530

    modified apple_and_orange.cpp

--- HackerRank/Algorithms/apple_and_orange.cpp
+++ HackerRank/Algorithms/apple_and_orange.cpp
@@ -9,13 +9,24 @@ using namespace std;
 int main() {
     int s, t, a, b, m, n;
     cin>>s>>t>>a>>b>>m>>n;
-    int apple[10000], orange[10000];   
+    int apple[10000], orange[10000];
+    int count_ap=0;int count_or=0;
+
     for(int i=0; i<m; i++){
         cin>>apple[i];
     }
-    for(int i=0; i<m; i++){
+    for(int i=0; i<n; i++){
         cin>>orange[i];
     }
-    
+    int diff1= s-a;
+    for(int i=0; i<m; i++){
+        if(apple[i]>0&&apple[i]>=diff1){ count_ap++;}
+    }
+    int diff2= b-t;
+    for(int i=0; i<n; i++){
+        if(orange[i]<0&&(-1*orange[i])>=diff2){ count_or++;}
+    }
+    cout<<count_ap<<endl;
+    cout<<count_or<<endl;
     return 0;
 }
