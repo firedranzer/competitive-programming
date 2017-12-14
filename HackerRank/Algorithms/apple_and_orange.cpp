068921c8d718ce90f9f9d5d33e0165a838642df0
firedranzer <rupeshharode@gmail.com>
2017-12-14 12:37:16 +0530

    Added program apple_and_orange.cpp

---
+++ HackerRank/Algorithms/apple_and_orange.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int s, t, a, b, m, n;
+    cin>>s>>t>>a>>b>>m>>n;
+    int apple[10000], orange[10000];   
+    for(int i=0; i<m; i++){
+        cin>>apple[i];
+    }
+    for(int i=0; i<m; i++){
+        cin>>orange[i];
+    }
+    
+    return 0;
+}
