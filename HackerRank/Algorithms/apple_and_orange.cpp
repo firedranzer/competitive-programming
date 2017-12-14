3259a22f0a23b24b3aec2da3ed539902a59536fa
firedranzer <rupeshharode@gmail.com>
2017-12-14 14:25:11 +0530

    modified program apple_and_orange.cpp

--- HackerRank/Algorithms/apple_and_orange.cpp
+++ HackerRank/Algorithms/apple_and_orange.cpp
@@ -7,24 +7,20 @@ using namespace std;
 
 
 int main() {
-    int s, t, a, b, m, n;
+    long int s, t, a, b, m, n;
     cin>>s>>t>>a>>b>>m>>n;
-    int apple[10000], orange[10000];
-    int count_ap=0;int count_or=0;
-
-    for(int i=0; i<m; i++){
-        cin>>apple[i];
+    long int apple, orange;
+    int count_ap=0;
+    int count_or=0;
+    for(long int i=0; i<m; i++){
+        cin>>apple;
+        //int dist_app = a + apple;
+        if(a+apple>=s && a+apple <=t){    count_ap++;}
     }
-    for(int i=0; i<n; i++){
-        cin>>orange[i];
-    }
-    int diff1= s-a;
-    for(int i=0; i<m; i++){
-        if(apple[i]>0&&apple[i]>=diff1){ count_ap++;}
-    }
-    int diff2= b-t;
-    for(int i=0; i<n; i++){
-        if(orange[i]<0&&(-1*orange[i])>=diff2){ count_or++;}
+    for(long int i=0; i<n; i++){
+        cin>>orange;
+        //int dist_or = b + orange;
+        if(b+orange>=s && b+orange <=t){    count_or++;}    
     }
     cout<<count_ap<<endl;
     cout<<count_or<<endl;
