4a22a3f84797bbbb84d0593982c179194a1041c1
firedranzer <rupeshharode@gmail.com>
2017-12-18 16:59:54 +0530

    modified program migratory_bird.cpp

--- HackerRank/Algorithms/migratory_bird.cpp
+++ HackerRank/Algorithms/migratory_bird.cpp
@@ -7,6 +7,16 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    return 0;
+    int n, max;
+    cin>>n;
+    int types[6]={0,0,0,0,0,0}, A[10000];
+    for(int i=0 ;i<n; i++){
+        cin>>A[i];
+        types[A[i]] += 1;
+    }
+    int max = types[0];
+    for(int i=0; i<n; i++){
+        if(types[i]>max){   max=types[i];}
+    }
+    cout<<max;
 }
