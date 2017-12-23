0157f6d44283a472f4e461d59e0c0271908048f2
firedranzer <rupeshharode@gmail.com>
2017-12-24 01:06:00 +0530

    Modified program bigger_is_greater.cpp

--- HackerRank/Algorithms/bigger_is_greater.cpp
+++ HackerRank/Algorithms/bigger_is_greater.cpp
@@ -7,16 +7,14 @@ using namespace std;
 
 
 int main() {
-    vector<string> s;
     int n;
     string ch;
     cin>>n;
     for(int i=0; i<n; i++){
         cin>>ch;
-        s.pushback(ch);
-        string s2 = next_permutation(s.begin(), s.end());
-        if(ch==s2){ cout<<"no answer"<<endl;}
-        else    cout<<"s2"<<endl;
+        bool check = next_permutation(ch.begin(), ch.end());
+        if(check==false){ cout<<"no answer"<<endl;}
+        else    cout<<ch<<endl;
     }
     return 0;
 }
