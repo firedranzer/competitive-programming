93764c6888a2f6859ad5ad5e08cd1c8e4f69ce0e
firedranzer <rupeshharode@gmail.com>
2018-01-08 23:45:46 +0530

    Modified super_reduced_string.cpp

--- HackerRank/Algorithms/super_reduced_string.cpp
+++ HackerRank/Algorithms/super_reduced_string.cpp
@@ -7,6 +7,19 @@ using namespace std;
 
 
 int main() {
-    
+    string s;
+    cin>>s;
+    for(int i=1; i<=s.length(); i++){
+        if(s[i]==s[i-1]){
+            s = s.substr(0, i-1) + s.substr(i+1);
+            i = 0;
+        }
+    }
+    if(s.length()==0){
+        cout<<"Empty String";
+    }
+    else{
+        cout<<s;
+    }
     return 0;
 }
