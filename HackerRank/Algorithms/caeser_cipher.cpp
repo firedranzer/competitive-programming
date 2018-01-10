0ae01b264c2f65ceca7f5ee34f2e7dac7be26cf8
firedranzer <rupeshharode@gmail.com>
2018-01-10 19:22:41 +0530

    Modified program caeser_cipher.cpp

--- HackerRank/Algorithms/caeser_cipher.cpp
+++ HackerRank/Algorithms/caeser_cipher.cpp
@@ -8,14 +8,21 @@ using namespace std;
 
 int main() {
     int n, k;
-    string S;
+    string S, res="";
     cin>>n;
     cin>>S;
     cin>>k;
     for(int i=0; i<n; i++){
-        int c = S[i];
-        c = char((int(c)+k)%26);
-        S[i] = c;
+        if(isupper(S[i])){
+            res += char(int(S[i] + k - 64)%26+64);
+            //cout<<S[i];
+        }
+        if(islower(S[i])){
+            res += char(int(S[i] + k - 96)%26+96);
+            //cout<<S[i];
+        }
+        else res=S[i];
     }
+    cout<<res;
     return 0;
 }
