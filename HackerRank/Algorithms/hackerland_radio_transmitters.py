6ad1aec084ee6fc0b76d34e4db56dd51cb23b11a
firedranzer <rupeshharode@gmail.com>
2018-01-11 11:25:22 +0530

    Modified program

--- HackerRank/Algorithms/hackerland_radio_transmitters.py
+++ HackerRank/Algorithms/hackerland_radio_transmitters.py
@@ -3,7 +3,15 @@
 import sys
 
 def hackerlandRadioTransmitters(x, k):
+    count = 1
+    x.sort()
+    begin = x[0]
+    for i in range(0, n):
+        if(x[i]>begin+2*k):
+            count = count + 1;
+            begin = x[i]
     
+    return count
 
 if __name__ == "__main__":
     n, k = input().strip().split(' ')
