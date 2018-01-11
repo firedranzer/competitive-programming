79d242494862f6e0cc4df555fb461c176b837dd7
firedranzer <rupeshharode@gmail.com>
2018-01-11 22:04:17 +0530

    Modified program hackerrank_in_a_string.cpp

--- HackerRank/Algorithms/hackerrank_in_a_string.cpp
+++ HackerRank/Algorithms/hackerrank_in_a_string.cpp
@@ -9,16 +9,18 @@ using namespace std;
 int main() {
     string A="hackerrank";
     string B;
-    int pos=0;
-    cin>>B;
-    int count=0;
-    for(int i=0; i<B.length(); i++){
-        
-        if(count <A.length() && B[i]==A[count]){
-            count++;
+    int q;
+    cin>>q;
+    while(q--){
+        cin>>B;
+        int count=0;
+        for(int i=0; i<B.length(); i++){
+            if(count <A.length() && B[i]==A[count]){
+                count++;
+            }
         }
+        if(count==10)    cout<<"YES"<<endl;
+        else    cout<<"NO"<<endl;
     }
-    if(count==10)    cout<<"YES"<<endl;
-    else    cout<<"NO"<<endl;
     return 0;
 }
