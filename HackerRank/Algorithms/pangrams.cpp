d626e97d9945f8ffa998095610d9cfbf9e564d08
firedranzer <rupeshharode@gmail.com>
2018-01-14 00:07:55 +0530

    Modified program pangram.cpp

--- HackerRank/Algorithms/pangrams.cpp
+++ HackerRank/Algorithms/pangrams.cpp
@@ -3,10 +3,32 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <string.h>
 using namespace std;
 
 
 int main() {
-    
+    string s;
+    getline(cin, s);
+    int a[26];
+    int count=0;
+    for(int i=0; i<s.length(); i++){
+        int x = tolower(s[i]);
+        if(isalpha(s[i])){
+            a[x-97]++;
+        }
+    }
+
+    for(int i=0; i<26; i++){
+        if(a[i]>0){
+            count++;
+        }
+    }
+    if(count==26){
+        cout<<"pangram";
+    }
+    else{
+        cout<<"not pangram";
+    }
     return 0;
 }
