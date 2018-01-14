4fe8aeb5b7dcfbc30a46db0a01b983f2988e6bb4
firedranzer <rupeshharode@gmail.com>
2018-01-14 08:51:00 +0530

    modified funny_strings.cpp

--- HackerRank/Algorithms/funny_string.cpp
+++ HackerRank/Algorithms/funny_string.cpp
@@ -7,6 +7,18 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string s;
+    cin>>s;
+    int i;
+    int n = s.length();
+    for( i=1, int j=n-i; i<=n/2, j>=n/2; i++, j--){
+        int diff_f = (s[i]-s[i-1]);
+        int diff_r = (s[j]-s[j-1]);
+        if(diff_f!=diff_r){
+            break;
+        }
+    }
+    if(i==n/2){ cout<<"Funny";}
+    else{   cout<<"Not Funny";}
     return 0;
 }
