644f9cc1273e2129a60dfd32707d34906e4ff4d5
firedranzer <rupeshharode@gmail.com>
2018-01-14 08:56:17 +0530

    modified funny_strings.cpp

--- HackerRank/Algorithms/funny_string.cpp
+++ HackerRank/Algorithms/funny_string.cpp
@@ -7,18 +7,22 @@ using namespace std;
 
 
 int main() {
-    string s;
-    cin>>s;
-    int i;
-    int n = s.length();
-    for( i=1, int j=n-i; i<=n/2, j>=n/2; i++, j--){
-        int diff_f = (s[i]-s[i-1]);
-        int diff_r = (s[j]-s[j-1]);
-        if(diff_f!=diff_r){
-            break;
+    int t, i, j;
+    int count=0;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int n = s.length();
+        for( i=1, j=n-i; i<=n/2, j>=n/2; i++, j--){
+            int diff_f = (s[i]-s[i-1]);
+            int diff_r = (s[j]-s[j-1]);
+            if(diff_f==diff_r){
+                count++;
+            }
         }
+        if(count==n/2){ cout<<"Funny\n";}
+        else{   cout<<"Not Funny\n";}
     }
-    if(i==n/2){ cout<<"Funny";}
-    else{   cout<<"Not Funny";}
     return 0;
 }
