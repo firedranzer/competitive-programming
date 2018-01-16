24bf841aa679776383adfe771b9dc11c57aaa192
firedranzer <rupeshharode@gmail.com>
2018-01-16 22:54:11 +0530

    Modified program the_maximum_subarray.cpp

--- HackerRank/Algorithms/the_maximum_subarray.cpp
+++ HackerRank/Algorithms/the_maximum_subarray.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 
 int main() {
-    int t, max_current, max_global;
+    int t, max_current, max_global, max_cur_seq, max_global_seq;
     cin>>t;
     while(t--){
         int sum=0;
         int n;cin>>n;
-        int a[10000];
+        long int a[100000];
         for(int i=0; i<n; i++){
             cin>>a[i];
         }
@@ -23,13 +23,18 @@ int main() {
                 max_global = max_current ; 
             }
         }
-        for(int i=0;  i<n; i++){
-            if(a[i]>0){
-                sum += a[i];
+        
+        sort(a, a+n, greater<int> ());
+        max_cur_seq = max_global_seq = a[0];
+        for(int i=1; i<n; i++){
+            max_cur_seq = max(a[i], max_cur_seq + a[i]);
+            if(max_cur_seq > max_global_seq){
+                max_global_seq = max_cur_seq ; 
             }
         }
+        
         cout<<max_global<<" ";
-        cout<<sum<<"\n";
+        cout<<max_global_seq<<"\n";
     }
     return 0;
 }
