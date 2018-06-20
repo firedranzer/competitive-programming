9109b3c81d99bbc38d6a96d4591f741b789202a6
firedranzer <rupeshharode@gmail.com>
2018-06-20 22:57:15 +0530

    modifies designerPdfViewer.py

---
+++ HackerRank/Algorithms/designerPdfViewer.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the designerPdfViewer function below.
+def designerPdfViewer(h, word):
+    max = 0
+    for char in word:
+        if(max<h[ord(char)-97]):
+            max = h[ord(char)-97]
+    return max*len(word)
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
