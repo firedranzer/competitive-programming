f68890b2977011c51d10451bd1ce9c29f5ff6172
firedranzer <rupeshharode@gmail.com>
2018-06-24 23:35:15 +0530

    modifies program strangeAdvertising.cpp

---
+++ HackerRank/Algorithms/strangeAdvertising.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the viralAdvertising function below.
+int viralAdvertising(int n) {
+    int like = 5/2;
+    int sum = 0;
+    for(int i=0; i<n-1; i++){
+        int spread = like*3;
+        like = int(spread/2);
+        sum += like; 
+    }
+    return sum+2;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
