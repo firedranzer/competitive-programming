7b79b4a614e8b83012d73cbb2f263ca7171f871c
firedranzer <rupeshharode@gmail.com>
2018-06-28 23:19:20 +0530

    modifies pickingNumbers.cpp

---
+++ HackerRank/Algorithms/pickingNumbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    std::cin >> n;
+    
+    vector<int> a(n);
+    for(auto& e:a){
+        cin >> e;
+    }
+    
+    sort(a.begin(),a.end());
+    vector<int> count(n, 0);
+    for(int i=0; i< a.size(); i++){
+        //count[i] = 0;
+        for(int j=1; j<a.size(); j++){
+            if(abs(a[i]-a[j])<=1){
+                count[i]++;
+            }
+            else
+                break;
+        }
+    }
+    
+    std::cout << *std::max_element(count.begin(), count.end());
+    return 0;
+}
