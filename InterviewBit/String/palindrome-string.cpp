92151d394bc0760dd158f09f3785f2330abd040a
firedranzer <rupeshharode@gmail.com>
2018-07-24 08:25:27 +0530

    adds program palindrome-string.cpp

---
+++ InterviewBit/String/palindrome-string.cpp
@@ -0,0 +1,14 @@
+//https://www.interviewbit.com/problems/palindrome-string/
+
+int Solution::isPalindrome(string A) {
+    int i = 0, j = A.length()-1;
+    while (i<j)
+    {
+        while(i<j && !isalnum(A[i])) i++;
+        while(i<j && !isalnum(A[j])) j--;
+        if(tolower(A[i])!=tolower(A[j]))
+            return 0;
+        i++; j--;
+    }
+    return 1;
+}
