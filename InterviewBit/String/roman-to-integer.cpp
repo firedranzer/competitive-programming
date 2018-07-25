f6e5b62532a1b74e870bd75b5636875de71ce3c7
firedranzer <rupeshharode@gmail.com>
2018-07-25 10:45:35 +0530

    adds program roman-to-integer.cpp

---
+++ InterviewBit/String/roman-to-integer.cpp
@@ -0,0 +1,41 @@
+//https://www.interviewbit.com/problems/roman-to-integer/
+
+int value(char r)
+{
+    if (r == 'I')
+        return 1;
+    if (r == 'V')
+        return 5;
+    if (r == 'X')
+        return 10;
+    if (r == 'L')
+        return 50;
+    if (r == 'C')
+        return 100;
+    if (r == 'D')
+        return 500;
+    if (r == 'M')
+        return 1000;
+ 
+    return -1;
+}
+
+int Solution::romanToInt(string A) {
+    int res = 0;
+    for(int i=0; i<A.length(); i++){
+        int s1 = value(A[i]);
+        
+        if(i+1<A.length()){
+            int s2 = value(A[i+1]);
+            if(s2>s1){
+                res = res + s2 - s1;
+            }else{
+                res = res + s1;
+            }
+        }else{
+            res = res + s1;
+        }
+        i++;
+    }
+    return res;
+}
