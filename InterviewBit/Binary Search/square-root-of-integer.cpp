9a1163d4c2a27572f1a38378980456807a6d8880
firedranzer <rupeshharode@gmail.com>
2018-07-30 19:41:58 +0530

    adds program square-root-of-integer.cpp

---
+++ InterviewBit/Binary Search/square-root-of-integer.cpp
@@ -0,0 +1,19 @@
+//https://www.interviewbit.com/problems/square-root-of-integer/
+
+int Solution::sqrt(int A) {
+    if (A==0 || A==1) return A;
+    int start = 0, end = A;
+    int ans;
+    while(start<=end)
+    {
+        int mid = (end + start)/2;
+        if (mid <= A/mid) 
+        {
+            start = mid + 1;
+            ans = mid;
+        }
+        else 
+            end = mid - 1;
+    }
+    return ans;
+}
