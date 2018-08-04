52b1d9280dc0603add9e376a8865f7d031021df4
firedranzer <rupeshharode@gmail.com>
2018-08-04 09:49:16 +0530

    modifies program merge-two-sorted-lists.cpp

---
+++ InterviewBit/LinkedList/merge-two-sorted-lists.cpp
@@ -0,0 +1,28 @@
+//https://www.interviewbit.com/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::mergeTwoLists(ListNode* A, ListNode* B) {
+    struct ListNode * result = NULL;
+    if(A==NULL){
+        return B;
+    }
+    else if(B==NULL){
+        return A;
+    }
+    if(A->val <= B->val){
+        result = A;
+        result->next = mergeTwoLists(A->next, B);
+    }
+    else{
+        result = B;
+        result->next = mergeTwoLists(A, B->next);
+    }
+    return result;
+}
