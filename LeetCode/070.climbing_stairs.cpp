751d9a009a5a10da6409f9b78a210fe9cf20d2e2
firedranzer <rupeshharode@gmail.com>
2018-08-29 19:28:46 +0530

    adds 070.climbing_stairs.cpp

---
+++ LeetCode/070.climbing_stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1){
+            return 1;
+        }
+        int first = 1; int second = 1;
+        int sum = 0;
+        for(int i=2; i<=n; i++){
+            sum = first + second;
+            first = second;
+            second = sum;
+        }
+        return sum;
+    }
+};
